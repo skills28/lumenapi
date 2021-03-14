3839516c5e4b59588f395622698377ffd8697e34
skills28 <80546161+skills28@users.noreply.github.com>
2021-03-14 23:25:58 +0500

    why
    this is lumen api

---
+++ database/seeders/PostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PostController extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
